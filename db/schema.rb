a19be226a9000131d71a445db63644bb9d35519a
Jake Levy <jake.r.levy@gmail.com>
2013-03-24 16:53:35 -0400

    remembering to git r done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320154204) do
+ActiveRecord::Schema.define(:version => 20130323132346) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
